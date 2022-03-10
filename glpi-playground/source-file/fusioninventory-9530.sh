cb0efce4873995bbffc4e813abbfe6b0c3179c41
vijai <vijai.djearam@gmail.com>
2022-03-10 14:28:00 +0100

    Update fusioninventory-9530.sh

--- glpi-playground/source-file/fusioninventory-9530.sh
+++ glpi-playground/source-file/fusioninventory-9530.sh
@@ -3,12 +3,10 @@ apt-get update -y
 apt-get upgrade -y
 cd /usr/src
 wget https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.5%2B3.0/fusioninventory-9.5+3.0.tar.bz2
-apt-get install bzip2
+apt-get install bzip2 -y
+apt-get install nano -y
 tar xjvf fusioninventory-9.5+3.0.tar.bz2
 chown -R www-data:www-data /var/www/html/glpi/plugins
 cd /var/www/html/glpi/plugins
 mv /usr/src/fusioninventory fusioninventory/
-crontab -u www-data -e
-* * * * *  /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null
-/etc/init.d/cron restart
 rm -R /usr/src/fusioninventory-9.5+3.0.tar.bz2
